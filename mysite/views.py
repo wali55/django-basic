7e618127efffd19bcf99a1146e544aa98d107866
wali55 <walisantunu@gmail.com>
2024-01-23 22:38:43 +0600

    show employees in the table

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from employee.models import Employee
 
 def home(request):
-    return render(request, 'home.html')
+    employees = Employee.objects.all()
+    context = {
+        'employees': employees,
+    }
+    return render(request, 'home.html', context)
