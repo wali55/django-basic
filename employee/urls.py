59c9ad416c6d6d18d449d4d1d7efb01c54ada5ec
wali55 <walisantunu@gmail.com>
2024-01-27 22:46:33 +0600

    employee detail

--- employee/urls.py
+++ employee/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.employee_detail)
+    path('<int:pk>/', views.employee_detail, name='employee_detail'),
 ]
